3a9ee7f0ec1e1d728fc75997b75a3dbd290bd4a2
Binh Hoang <hoangb3299@my.sduhsd.net>
2026-05-17 05:50:56 +0000

    Added backgrounds, and movement such as jumping

--- sketch.js
+++ sketch.js
@@ -7,55 +7,65 @@ ball.img = '🤪';
 
 let groundA = new Sprite();
 groundA.x = -120;
-groundA.width = 220;
+groundA.width = 500;
 groundA.rotation = 0;
+groundA.y = 300;
+groundA.fill="#f065ef";
+groundA.stroke="#f065ef";
 groundA.physics = STATIC;
 
 let groundB = new Sprite();
-groundB.x = 120;
-groundB.width = 220;
-groundB.rotation = 30;
+groundB.x = 400;
+groundB.width = 270;
+groundB.y = 20;
+groundB.fill="#f065ef";
+groundB.stroke="#f065ef";
+groundB.rotation = 0;
 groundB.physics = STATIC;
 
-q5.update = function () {
-	background('skyblue');
-	text('click to jump!', 0, -50);
+let groundC = new Sprite();
+groundC.x = -350;
+groundC.width = 350;
+groundC.y = -150;
+groundC.fill="#f065ef";
+groundC.stroke="#f065ef";
+groundC.rotation = 0;
+groundC.physics = STATIC;
 
-	if (mouse.presses()) 
-		{ball.vel.y = -5;}
 
-// test
-	if (kb.presses('w')) {
-	background('lime');}
-
-	//move right
-	if (kb.presses('d')) {
-	player.vel.x = 5;
-}
-	else{
-		player.vel.x = 0;
-	}
-	//move left
-	if (kb.presses('a')) {
-	player.vel.x = -5;
-}
-	else{
-		player.vel.x = 0;
-};
-}
-
+let bg = await loadImage('mainbg.jpg');
 let playersheet = await loadImage("notkirby.png");
 	
 let player = new Sprite();
 player.x=0;
 player.y=0;
 
-player.addAni({
-		sheet: playersheet,
-		frames: 5,
-		cols:2,
-		rows:3
-	});
-player.scale = 3;
+player.img=playersheet;
+player.scale = 0.5;
+
+
+
+q5.update = function () {
+image(bg,0,0);
+
+	if (mouse.presses()) 
+		{ball.vel.y = -5;}
+
+// jump
+	if (kb.presses('w')) {
+	player.vel.y = -10;}
+
+
+	//move left or right
+	if (kb.pressing('d')) {
+	player.vel.x = 10;
+}
+	else if (kb.pressing('a')) {
+	player.vel.x = -10;
+}
+	else{
+		player.vel.x = 0;
+}}
+
 
 
