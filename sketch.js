06b1f398213b6f24e1ec6b6be5b5aeb7d4a0adc3
Binh Hoang <hoangb3299@my.sduhsd.net>
2026-05-18 03:12:15 +0000

    Bullets functions for main character to shoot

--- sketch.js
+++ sketch.js
@@ -1,10 +1,8 @@
 await Canvas();
 world.gravity.y = 10;
 
-let ball = new Sprite();
-ball.diameter = 50;
-ball.img = '🤪';
 
+//platforms
 let groundA = new Sprite();
 groundA.x = -120;
 groundA.width = 500;
@@ -32,10 +30,11 @@ groundC.stroke="#f065ef";
 groundC.rotation = 0;
 groundC.physics = STATIC;
 
+// sprite load
+let bg = await loadImage('mainbg.jpg'); // fighting background
+let playersheet = await loadImage("notkirby.png"); //sprite fighter
+let bulletimage =  await loadImage("bullet.png"); //bullet
 
-let bg = await loadImage('mainbg.jpg');
-let playersheet = await loadImage("notkirby.png");
-	
 let player = new Sprite();
 player.x=0;
 player.y=0;
@@ -43,29 +42,55 @@ player.y=0;
 player.img=playersheet;
 player.scale = 0.5;
 
+// bullet direction:
+let direction = 1;
 
-
+//movment functions
 q5.update = function () {
 image(bg,0,0);
 
-	if (mouse.presses()) 
-		{ball.vel.y = -5;}
-
-// jump
+// playerjump
 	if (kb.presses('w')) {
 	player.vel.y = -10;}
 
 
 	//move left or right
-	if (kb.pressing('d')) {
+	if (kb.pressing('d')) { //right
 	player.vel.x = 10;
+	direction=1;
+	player.scale.x = 0.5;
 }
-	else if (kb.pressing('a')) {
+	else if (kb.pressing('a')) { //left
 	player.vel.x = -10;
+	direction=-1;
+	player.scale.x = -0.5;
 }
 	else{
 		player.vel.x = 0;
-}}
+}
+
+//shoot bullets
+if (kb.presses('s')){
+let bullethit = new Sprite();
+
+bullethit.x=player.x;
+bullethit.y=player.y;
+
+bullethit.w =10;
+bullethit.h= 10;
 
 
+bullethit.img=bulletimage;
+bullethit.scale = 2.5;
 
+bullethit.vel.x = 15*direction;
+
+if (direction === -1){
+	bullethit.scale.x=-2.5
+}
+else{
+	bullethit.scale.x=2.5
+}
+
+}
+}
